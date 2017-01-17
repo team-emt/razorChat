abcb453650a0b44edec3213f1f8402c91374136a
Michael Sotkin <michael.sotkin@gmail.com>
2017-01-17 14:12:18 -0800

    Style: added icons to cards

--- app/components/Notecard.jsx
+++ app/components/Notecard.jsx
@@ -2,11 +2,19 @@ import React, { PropTypes, Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 class Notecard extends Component {
-    render() {
-        return (
-            <div className="notecard" >{ this.props.msg }</div>
-        )
-    }
+  render() {
+    return (
+      <div className="notecard">
+        <div><strong>Username</strong> @username</div>
+        <div>{ this.props.msg }</div>
+        <div>
+          <div className="iconwrap"><img className="icon" src={require('./../assets/reply.png')}/></div>
+          <div className="iconwrap"><img className="icon" src={require('./../assets/retweet.png')}/></div>
+          <div className="iconwrap"><img id="heart" src={require('./../assets/heart.png')}/></div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Notecard;
