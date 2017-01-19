95fcd35d1d519d115ac6ef9c318574cc0c593da9
travishuff <huff.travis@gmail.com>
2017-01-19 11:38:20 -0800

    Feat: Adds Redux

--- app/components/ChatInput.jsx
+++ app/components/ChatInput.jsx
@@ -1,9 +1,9 @@
 import React, { PropTypes } from 'react';
 
-const ChatInput = ({ inputValue, sendChat, handleChange }) => {
+const ChatInput = ({ inputValue, sendMsg, handleChange }) => {
   return (
     <div className="form-chat">
-      <form id="text-input-form" onSubmit={sendChat}>
+      <form id="text-input-form" onSubmit={sendMsg}>
         <div>
           <input type="text" id="text-input-field" className="input-chat" value={inputValue} onChange={handleChange} />
           <button type="submit" className="submit-button">Submit</button>
@@ -14,9 +14,9 @@ const ChatInput = ({ inputValue, sendChat, handleChange }) => {
 }
 
 ChatInput.propTypes = {
-  inputValue: React.PropTypes.string,
-  sendChat: React.PropTypes.func,
-  handleChange: React.PropTypes.func
+  inputValue: PropTypes.string,
+  sendMsg: PropTypes.func,
+  handleChange: PropTypes.func
 };
 
 export default ChatInput;
